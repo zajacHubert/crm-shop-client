4a12d9ad583cbf95d35887c2f2de50168edd2bdd
Hubert <hubert.zajac92@gmail.com>
2023-04-25 08:09:29 +0200

    CSC-1: Add styled components to project

---
+++ src/components/global/GlobalStyle.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+
+ul {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+}    
+`;
+
+export default GlobalStyle;
