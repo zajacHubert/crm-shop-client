805e91c7f2426dd9f0817621dc7c90e773e42e9e
Hubert <hubert.zajac92@gmail.com>
2023-05-06 10:58:30 +0200

    CSC-4: Add basic ored interfaces

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,5 +1,22 @@
+import { Product } from './product';
+
 export interface Order {
   id: string;
   user_id: string;
   created_at: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    role_id: string;
+  };
+  products: Product[];
+}
+
+export interface GetOrdersResponse {
+  current_page: number;
+  data: Order[];
+  per_page: number;
+  last_page: number;
+  total: number;
 }
